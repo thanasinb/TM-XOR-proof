02ba6a306bf8747f975e8c4c9e040f4da9150fac
thanasinb <thanasin.b@gmail.com>
2025-12-05 20:01:45 +0700

    implement 1T1M model

---
+++ model_1t1m.pyx
@@ -0,0 +1,90 @@
+# cython: language_level=3
+
+cdef class Model1T1M:
+    def __init__(self,
+                 int ta_state,
+                 float init_memristor_state,
+                 int number_of_states,
+                 float alpha_off,
+                 float alpha_on,
+                 float v_off,
+                 float v_on,
+                 float r_off,
+                 float r_on,
+                 float k_off,
+                 float k_on,
+                 float d,
+                 int clause,
+                 int feature,
+                 int negated,
+				 float rt_off,
+                 float rt_on,
+                 float dt_off,
+                 float dt_on,
+                 float voltage,
+                 float rm):
+
+        self.ta_state = ta_state
+        self.init_memristor_state = init_memristor_state
+        self.number_of_states = number_of_states
+        self.alpha_off = alpha_off
+        self.alpha_on = alpha_on
+        self.v_off = v_off
+        self.v_on = v_on
+        self.r_off = r_off
+        self.r_on = r_on
+        self.k_off = k_off
+        self.k_on = k_on
+        self.d = d
+        self.dx = 0
+        self.rt_off = rt_off
+        self.rt_on = rt_on
+        self.dt_off = dt_off
+        self.dt_on = dt_on
+        self.voltage = voltage
+        self.rm = rm
+        self.delta_rm_off = 0
+        self.delta_rm_2_off = 0
+        self.delta_rm_on = 0
+        self.delta_rm_2_on = 0
+
+        self.mr_state = (ta_state/self.number_of_states)*self.init_memristor_state
+        self.x = self.mr_state * self.d
+
+        self.delta_rm_1 = (r_off - r_on) / d
+        self.delta_rm_3_off = k_off * dt_off
+        self.delta_rm_3_on = k_on * dt_on
+
+    def tune(self, int off=0, int on=0):
+        if off == 1:
+            self.delta_rm_2_off = ((((self.rm * self.voltage) / (self.rm + self.rt_off)) / self.v_off) - 1) ** self.alpha_off
+            self.delta_rm_off = self.delta_rm_1 * self.delta_rm_2_off * self.delta_rm_3_off
+            self.rm += self.delta_rm_off
+            self.dx = self.delta_rm_off
+
+        elif on == 1:
+            self.delta_rm_2_on = ((((self.rm * self.voltage) / (self.rm + self.rt_on)) / self.v_on) - 1) ** self.alpha_on
+            self.delta_rm_on = self.delta_rm_1 * self.delta_rm_2_on * self.delta_rm_3_on
+            self.rm -= self.delta_rm_on
+            self.dx = self.delta_rm_on
+
+        if self.rm > self.r_off:
+            self.rm = self.r_off
+
+        if self.rm < self.r_on:
+            self.rm = self.r_on
+
+        self.mr_state = (self.rm - self.r_on) / (self.r_off - self.r_on)
+        self.ta_state = int(self.mr_state*self.number_of_states/self.init_memristor_state)
+
+    def get_mr_value(self):
+        return self.rm
+
+    def get_mr_state(self):
+        return self.mr_state
+
+    def get_mr_xdx(self):
+        return self.ta_state, self.mr_state, self.dx, self.rm
+
+    def get_ta_state(self):
+        return int(self.mr_state*self.number_of_states/self.init_memristor_state)
---
+++ model_1t1m_test2.py
@@ -0,0 +1,63 @@
+# cython: language_level=3
+
+import numpy as np
+import pyximport; pyximport.install(setup_args={
+                              "include_dirs":np.get_include()},
+                            reload_support=True)
+from model_1t1m import Model1T1M
+import vteam_params
+
+number_of_states = 100
+ta_state = 0
+init_memristor_state = 0.5
+voltage = 1.2
+rt_off = 2160
+rt_on = 5600
+
+selected_params = vteam_params.get_vteam_params("Seiler2024")
+alpha_off = selected_params["alpha_off"]
+alpha_on = selected_params["alpha_on"]
+v_off = selected_params["v_off"]
+v_on = selected_params["v_on"]
+r_off = selected_params["r_off"]
+r_on = selected_params["r_on"]
+k_off = selected_params["k_off"]
+k_on = selected_params["k_on"]
+d = selected_params["d"]
+dt_off = 10 * (10 ** -9)
+dt_on = 40 * (10 ** -9)
+
+clause = 0
+feature = 0
+negated = 0
+rm = r_on
+
+my_1t1m = Model1T1M(ta_state,
+                    init_memristor_state,
+                    number_of_states,
+                    alpha_off,
+                    alpha_on,
+                    v_off,
+                    v_on,
+                    r_off,
+                    r_on,
+                    k_off,
+                    k_on,
+                    d,
+                    clause,
+                    feature,
+                    negated,
+                    rt_off,
+                    rt_on,
+                    dt_off,
+                    dt_on,
+                    voltage,
+                    rm)
+
+for i in range(100):
+    my_1t1m.tune(1,0)
+    print(i+1, my_1t1m.get_mr_xdx())
+
+for i in range(100):
+    my_1t1m.tune(0,1)
+    print(i+1, my_1t1m.get_mr_xdx())
